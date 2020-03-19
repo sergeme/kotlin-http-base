69437964019c723de0d2521e769102b18188fb0d
Serge Meyer <40215327+bele321@users.noreply.github.com>
2020-03-19 17:33:20 +0100

    wip

--- src/me/sersch/http/services/auth/Auth.kt
+++ src/me/sersch/http/services/auth/Auth.kt
@@ -5,7 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm
 import java.io.FileInputStream
 import java.util.*
 
-open class Auth(val secret: String) {
+open class Auth(private val secret: String) {
     private val algorithm = Algorithm.HMAC256(secret)
     val verifier = JWT.require(algorithm).build()
     fun sign(name: String): String = JWT.create().withClaim("name", name).sign(algorithm)
